fe27bf956588c04f5c63465d53a38e6ba7f61861
Timofey.Kirillov <timofey.kirillov@flant.ru>
2013-06-26 20:57:00 +0400

    setup.py++

--- setup.py
+++ setup.py
@@ -4,5 +4,7 @@ setup(
     name='overseed',
     description='Filesystem inotify-based watcher',
     version='0.1',
+    author='Timofey Kirillov',
+    author_email='distorhead@gmail.com',
     scripts=['overseed']
 )
